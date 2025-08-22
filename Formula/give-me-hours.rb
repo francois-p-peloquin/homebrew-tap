9c6019f761c14bc99e4cf843eb2909dc4e0633aa
Francois Peloquin <francois.p.peloquin@gmail.com>
2025-08-22 13:05:05 -0700

    Updated

--- Formula/give-me-hours.rb
+++ Formula/give-me-hours.rb
@@ -1,14 +1,12 @@
 class GiveMeHours < Formula
   desc "Track git commit hours across multiple repositories"
   homepage "https://github.com/francois-p-peloquin/give-me-hours"
-  url "https://github.com/francois-p-peloquin/give-me-hours/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "YOUR_SHA256_HASH_HERE"
+  url "https://github.com/francois-p-peloquin/give-me-hours/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "a2e3f894961963b2d04fe5e3a8bd569af42efb8f802a828830111104e70086d7"
   license "MIT"
-  version "1.0.0"
+  version "1.0.1"
 
   depends_on "node"
-  depends_on "git"
-  depends_on "git-hours" => :optional
 
   def install
     bin.install "give-me-hours"
@@ -16,15 +14,15 @@ class GiveMeHours < Formula
 
   def caveats
     <<~EOS
-      This tool requires git-hours to be installed.
-      Install it with: npm install -g git-hours
+      Before using give-me-hours, install git-hours:
+        npm install -g git-hours
 
-      Make sure your git global username is set:
-      git config --global user.name "Your Name"
+      Set your git username if not already set:
+        git config --global user.name "Your Name"
     EOS
   end
 
   test do
-    system "#{bin}/give-me-hours", "--help"
+    system bin/"give-me-hours", "--help"
   end
 end
